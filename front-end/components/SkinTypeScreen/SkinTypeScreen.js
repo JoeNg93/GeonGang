39b088b90f3506229591627f2c3e4a1c5bb21b9b
JoeNg93 <ntuandung93@gmail.com>
2018-03-19 17:34:46 +0200

    Update example component

--- front-end/components/SkinTypeScreen/SkinTypeScreen.js
+++ front-end/components/SkinTypeScreen/SkinTypeScreen.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const SkinTypeScreen = () => {
-  return <h1>Skin Type Screen</h1>;
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'montserrat-light' }}>Skin Type Screen</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default SkinTypeScreen;
