b60e32bdcffa10f1f97ac1198d0f60c041655fc9
JoeNg93 <ntuandung93@gmail.com>
2018-03-19 17:34:34 +0200

    Add some fonts and images

--- front-end/App.js
+++ front-end/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { Font, Asset } from 'expo';
 import store from './store';
 
 export default class App extends Component {
+  state = {
+    assetLoaded: false
+  };
+
+  componentDidMount = async () => {
+    // Load Font
+    await Font.loadAsync({
+      'montserrat-light': require('./assets/fonts/Montserrat-Light.otf'),
+      'montserrat-medium': require('./assets/fonts/Montserrat-Medium.otf'),
+      'montserrat-regular': require('./assets/fonts/Montserrat-Regular.otf'),
+      'montserrat-semibold': require('./assets/fonts/Montserrat-SemiBold.otf')
+    });
+
+    // Load images
+    await Asset.loadAsync(require('./assets/images/normal_skin.png'));
+    await Asset.loadAsync(require('./assets/images/oily_skin.png'));
+    await Asset.loadAsync(require('./assets/images/sensitive_skin.png'));
+
+    this.setState({ assetLoaded: true });
+  };
+
   render() {
-    return (
-      <Provider store={store}>
+    if (this.state.assetLoaded) {
+      return (
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </Provider>
+      );
+    } else {
+      return (
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Loading...</Text>
         </View>
-      </Provider>
-    );
+      );
+    }
   }
 }
 
