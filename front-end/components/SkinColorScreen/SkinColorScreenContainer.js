e094be1824baf64562c449224e6e8ac951923a1a
rwbarkeriv <rwbarkeriv@gmail.com>
2018-03-22 20:48:00 +0200

    Prettier

--- front-end/components/SkinColorScreen/SkinColorScreenContainer.js
+++ front-end/components/SkinColorScreen/SkinColorScreenContainer.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import SkinColorScreen from './SkinColorScreen';
 
 class SkinColorScreenContainer extends Component {
-    state = {
-        color: 50,
-        colorText: 'Medium'
-    };
+  state = {
+    color: 50,
+    colorText: 'Medium'
+  };
 
-    render() {
-        return (
-            <SkinColorScreen
-                color={this.state.color}
-                colorText={this.state.colorText}
-                onColorChange={
-                    newColor => this.setState({ color: newColor })}
-            />
-        );
-    }
+  render() {
+    return (
+      <SkinColorScreen
+        color={this.state.color}
+        colorText={this.state.colorText}
+        onColorChange={newColor => this.setState({ color: newColor })}
+      />
+    );
+  }
 }
 
 export default SkinColorScreenContainer;
