89fd28a7d8895e86b748dc8770192f44186fe6f1
Maxime Marmont <marmontm@users.noreply.github.com>
2018-03-26 02:38:51 +0300

    Create Scanning Screen (with static value)

--- front-end/components/ScanningScreen/ScanningScreen.js
+++ front-end/components/ScanningScreen/ScanningScreen.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-elements';
-import commonStyles from '../../utils/styles';
-import Header from '../common/Header';
-
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Button } from "react-native-elements";
+import commonStyles from "../../utils/styles";
+import Header from "../common/Header";
 
 const ScanningScreen = () => {
   return (
     <View style={styles.container}>
-        <Header
-        headerText="Skin analyzing"
-        descriptionText=""
+      <Header headerText="Skin analyzing" descriptionText="" />
+      <Text
+        style={[
+          commonStyles.fontMontserratLight,
+          commonStyles.colorDarkBlue,
+          styles.informationText
+        ]}
+      >
+        Hold on.{"\n"}
+        We are scanning your skin.{"\n"}
+        It will take just a minute.
+      </Text>
+      <View style={styles.loading}>
+        <Image
+          source={require("../../assets/images/scan-loading-1.png")}
+          style={styles.loadingImage}
         />
-        <View>
-            
-        </View>
+        <Text
+          style={[
+            commonStyles.fontMontserratRegular,
+            commonStyles.colorDarkBlue,
+            styles.loadingText
+          ]}
+        >
+          52%
+        </Text>
+      </View>
     </View>
   );
 };
@@ -22,9 +41,34 @@ const ScanningScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'stretch'
+    flexDirection: "column"
+    // justifyContent: 'space-between',
+    // alignItems: 'stretch'
+  },
+
+  informationText: {
+    fontSize: 17,
+    textAlign: "center",
+    paddingVertical: 32
+    // backgroundColor: '#bababa'
+  },
+
+  loading: {
+    flex: 2,
+    justifyContent: "center"
+    // backgroundColor: '#dadada'
+  },
+
+  loadingImage: {
+    resizeMode: "contain",
+    height: 193,
+    width: undefined
+  },
+
+  loadingText: {
+    fontSize: 48,
+    textAlign: "center",
+    marginTop: 32
   }
 });
 
