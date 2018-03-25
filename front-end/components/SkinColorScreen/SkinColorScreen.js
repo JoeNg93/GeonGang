f379f34d57eb0b716dc19e40eab20e35c579c090
rwbarkeriv <rwbarkeriv@gmail.com>
2018-03-25 21:20:10 +0300

    Add HeaderWithLogo component

--- front-end/components/SkinColorScreen/SkinColorScreen.js
+++ front-end/components/SkinColorScreen/SkinColorScreen.js
@@ -2,60 +2,65 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Slider } from 'react-native-elements';
 import commonStyles from '../../utils/styles';
+import HeaderWithLogo from '../common/HeaderWithLogo';
 
 const SkinColorScreen = ({ color, colorText, onColorChange }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.colorContainer}>
-        <View
-          backgroundColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
-          style={styles.colorImage}
+    <View>
+      <HeaderWithLogo headerText="Your skin color" />
+      <View style={styles.container}>
+        <View style={styles.colorContainer}>
+          <View
+            backgroundColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
+            style={styles.colorImage}
+          />
+          <Text style={[styles.colorText, commonStyles.fontMontserratRegular]}>
+            {colorText}
+          </Text>
+        </View>
+        <Slider
+          minimumTrackTintColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
+          maximumTrackTintColor="#C4C4C4"
+          thumbTintColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
+          trackStyle={{
+            height: 8,
+            borderRadius: 0
+          }}
+          thumbStyle={{ height: 27, width: 27, borderRadius: 50 }}
+          style={styles.slider}
+          step={1}
+          minimumValue={3}
+          maximumValue={100}
+          value={color}
+          onValueChange={onColorChange}
         />
-        <Text style={[styles.colorText, commonStyles.fontMontserratRegular]}>
-          {colorText}
-        </Text>
       </View>
-      <Slider
-        minimumTrackTintColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
-        maximumTrackTintColor="#C4C4C4"
-        thumbTintColor={'hsl(25, 55%, ' + (100 - color) + '%)'}
-        trackStyle={{
-          height: 8,
-          borderRadius: 0
-        }}
-        thumbStyle={{ height: 27, width: 27, borderRadius: 50 }}
-        style={styles.slider}
-        step={1}
-        minimumValue={3}
-        maximumValue={100}
-        value={color}
-        onValueChange={onColorChange}
-      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
     alignItems: 'center',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    paddingTop: 100
   },
   colorContainer: {
     alignItems: 'center',
     justifyContent: 'center'
   },
   colorImage: {
-    width: 95,
-    height: 95,
-    borderRadius: 50
+    width: 105,
+    height: 105,
+    borderRadius: 100
   },
   colorText: {
     fontSize: 26,
     color: '#4F4F4F',
-    paddingTop: 17,
-    paddingBottom: 22
+    paddingTop: 20,
+    paddingBottom: 35
   },
   slider: {
     width: 344,
