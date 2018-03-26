2c01879356c463d82cfe2739424e5cb3df2f4225
rwbarkeriv <rwbarkeriv@gmail.com>
2018-03-26 18:27:51 +0300

    Add initial screen structure

---
+++ front-end/components/UserProfileScreen/UserProfileScreenContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import UserProfileScreen from './UserProfileScreen';
+
+class UserProfileScreenContainer extends Component {
+  render() {
+    return <UserProfileScreen />;
+  }
+}
+
+export default UserProfileScreenContainer;
