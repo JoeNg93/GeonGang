370eb81badcf43a35dec903b4909c74dc394d978
rwbarkeriv <rwbarkeriv@gmail.com>
2018-03-30 20:04:42 +0300

    Rough structure of button group

--- front-end/components/UserProfileScreen/UserProfileScreen.js
+++ front-end/components/UserProfileScreen/UserProfileScreen.js
@@ -1,23 +1,82 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-import { Button, Icon } from 'react-native-elements';
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { Button, Icon, ButtonGroup, Divider } from 'react-native-elements';
 import commonStyles from '../../utils/styles';
 
-const UserProfileScreen = () => {
+const UserProfileScreen = buttons => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Icon name="bell" type="simple-line-icon" color="#000" size={22} />
-        <Icon name="settings" type="simple-line-icon" color="#000" size={22} />
+        <Icon
+          name="bell"
+          type="simple-line-icon"
+          color="#000"
+          size={22}
+          iconStyle={styles.headerButtons}
+        />
+        <Icon
+          name="settings"
+          type="simple-line-icon"
+          color="#000"
+          size={22}
+          iconStyle={styles.headerButtons}
+        />
 
         <Image
           style={styles.image}
           source={require('../../assets/images/Ellipse.png')}
         />
       </View>
-      <View>
-        <Icon name="pencil" type="simple-line-icon" color="#fff" size={22} />
+      <View style={styles.editAvatarIconContainer}>
+        <Icon
+          name="pencil"
+          type="simple-line-icon"
+          color="#fff"
+          size={15}
+          containerStyle={styles.editAvatarButton}
+          innerBorderStyle={{ width: 0 }}
+        />
       </View>
+      <Text
+        style={[
+          styles.userName,
+          commonStyles.colorDarkBlue,
+          commonStyles.fontMontserratRegular
+        ]}
+      >
+        Jennifer Aniston
+      </Text>
+      <View>
+        <ButtonGroup
+          buttons={['Profile', 'Friends', 'Reviews']}
+          selectedIndex={0}
+          containerStyle={styles.buttonGroupContainer}
+          buttonStyle={styles.buttonGroupButtons}
+          selectedButtonStyle={styles.selectedButton}
+          textStyle={styles.buttonGroupText}
+          selectedTextStyle={styles.selectedButtonText}
+        />
+      </View>
+      <Text
+        style={[
+          styles.productText,
+          commonStyles.colorDarkBlue,
+          commonStyles.fontMontserratRegular
+        ]}
+      >
+        My Products
+      </Text>
+      <Button
+        icon={
+          <Icon
+            name="arrow-right"
+            type="simple-line-icon"
+            size={20}
+            color="white"
+          />
+        }
+        title={'See all'}
+      />
     </View>
   );
 };
@@ -26,7 +85,8 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     alignItems: 'center',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    height: '100%'
   },
   header: {
     display: 'flex',
@@ -37,20 +97,27 @@ const styles = StyleSheet.create({
     height: 180,
     backgroundColor: '#C8EAFF'
   },
-  button: {
-    padding: 0,
-    marginTop: 35,
-    marginRight: 15
+  headerButtons: {
+    paddingTop: 35,
+    paddingRight: 15
   },
   headerButtonContainer: {
     marginLeft: 0,
     marginRight: 0
   },
-  editAvatarButtonContainer: {
-    marginLeft: 0,
-    marginRight: 0,
-    left: 55,
-    top: 30
+  editAvatarIconContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    width: 140,
+    height: 55,
+    marginBottom: 25
+  },
+  editAvatarButton: {
+    borderRadius: 50,
+    height: 27,
+    width: 27,
+    backgroundColor: '#4396DC'
   },
   image: {
     height: 140,
@@ -60,14 +127,35 @@ const styles = StyleSheet.create({
     right: 132,
     top: 105
   },
-  editAvatarButton: {
-    borderRadius: 50,
-    height: 32,
-    width: 32,
-    padding: 0,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column'
+  userName: {
+    fontSize: 26
+  },
+  buttonGroupContainer: {
+    height: 50,
+    width: '90%',
+    borderTopColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#E0E0E0',
+    borderBottomWidth: 4,
+    backgroundColor: '#fff'
+  },
+  buttonGroupButtons: {},
+  buttonGroupText: {
+    color: '#828282',
+    fontSize: 18
+  },
+  selectedButton: {
+    borderBottomWidth: 4,
+    borderBottomColor: '#4396DC'
+  },
+  selectedButtonText: {
+    color: '#4396DC'
+  },
+  productText: {
+    alignSelf: 'flex-start',
+    paddingLeft: 20,
+    fontSize: 20
   }
 });
 
