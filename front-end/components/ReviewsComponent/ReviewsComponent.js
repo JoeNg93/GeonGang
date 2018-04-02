bb1143713c09f7d9d8bf57942adcf6c56ba38ee7
rwbarkeriv <rwbarkeriv@gmail.com>
2018-04-03 00:11:55 +0300

    Add More Reviews Modal

--- front-end/components/ReviewsComponent/ReviewsComponent.js
+++ front-end/components/ReviewsComponent/ReviewsComponent.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { List, ListItem, Avatar, Rating, Button } from 'react-native-elements';
+import { StyleSheet, Text, View, Modal } from 'react-native';
+import {
+  List,
+  ListItem,
+  Avatar,
+  Rating,
+  Button,
+  Icon
+} from 'react-native-elements';
 import commonStyles from '../../utils/styles';
 
-const ReviewsComponent = ({ numOfReviews, list }) => {
+const ReviewsComponent = ({
+  numOfReviews,
+  list,
+  modalVisible,
+  setModalVisible
+}) => {
   return (
     <View style={styles.container}>
       <Text style={[styles.numberOfReviews, commonStyles.fontMontserratLight]}>
@@ -53,7 +65,78 @@ const ReviewsComponent = ({ numOfReviews, list }) => {
         containerViewStyle={styles.buttonContainer}
         buttonStyle={styles.button}
         textStyle={styles.textStyle}
+        onPress={() => {
+          setModalVisible(!modalVisible);
+        }}
       />
+
+      <Modal animationType="fade" transparent visible={modalVisible}>
+        <View style={styles.modalScreen}>
+          <View style={styles.modalContainer}>
+            <Icon
+              name="times"
+              type="font-awesome"
+              color="#828282"
+              size={25}
+              iconStyle={{
+                alignSelf: 'flex-end',
+                marginRight: 21,
+                marginTop: 17
+              }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            />
+            <Text
+              style={[
+                styles.numberOfReviewsModal,
+                commonStyles.fontMontserratLight
+              ]}
+            >
+              {numOfReviews} Reviews
+            </Text>
+            <List containerStyle={styles.list}>
+              {list.map((l, i) => (
+                <ListItem
+                  avatar={<Avatar medium source={{ uri: l.avatar_url }} />}
+                  key={i}
+                  title={l.name}
+                  subtitle={
+                    <View style={styles.subtitleView}>
+                      <Rating
+                        imageSize={14}
+                        readonly
+                        startingValue={l.rating}
+                        style={styles.rating}
+                      />
+                      <Text
+                        numberOfLines={2}
+                        style={[
+                          styles.listItemSubtitle,
+                          commonStyles.fontMontserratLight
+                        ]}
+                      >
+                        {l.info}
+                      </Text>
+                    </View>
+                  }
+                  containerStyle={styles.modalListItem}
+                  titleStyle={[
+                    styles.listItemTitle,
+                    commonStyles.fontMontserratLight,
+                    commonStyles.colorDarkBlue
+                  ]}
+                  subtitleStyle={[
+                    styles.listItemSubtitle,
+                    commonStyles.fontMontserratLight
+                  ]}
+                  subtitleNumberOfLines={2}
+                />
+              ))}
+            </List>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -71,7 +154,14 @@ const styles = StyleSheet.create({
   numberOfReviews: {
     alignSelf: 'flex-start',
     color: '#828282',
-    fontSize: 16
+    fontSize: 16,
+    paddingLeft: 11
+  },
+  numberOfReviewsModal: {
+    alignSelf: 'flex-start',
+    color: '#828282',
+    fontSize: 16,
+    paddingLeft: 22
   },
   list: {
     borderTopWidth: 0,
@@ -85,6 +175,14 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     paddingBottom: 20,
     paddingRight: 0
+  },
+  modalListItem: {
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    paddingTop: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 20
   },
   listItemTitle: {
     fontSize: 12,
@@ -116,6 +214,22 @@ const styles = StyleSheet.create({
   textStyle: {
     color: '#828282',
     fontSize: 14
+  },
+  modalScreen: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6);'
+  },
+  modalContainer: {
+    height: '75%',
+    width: '90%',
+    backgroundColor: '#fff',
+    borderRadius: 13,
+    shadowOffset: { width: 4, height: 4 },
+    shadowColor: 'black',
+    shadowOpacity: 0.25
   }
 });
 
