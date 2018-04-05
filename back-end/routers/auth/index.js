0da7136726ae1c2bf8e1cca8f843748d173733d3
JoeNg93 <ntuandung93@gmail.com>
2018-04-05 11:21:21 +0300

    Add API for signup

---
+++ back-end/routers/auth/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const knex = require('../../utils/knex_config');
+const router = express.Router();
+
+const requireInputs = (...inputs) => (req, res, next) => {
+  const missingInputs = inputs.filter(input => !req.body[input]);
+  if (missingInputs.length) {
+    res
+      .status(400)
+      .send({ error: `Missing arguments: ${missingInputs.join(', ')}` });
+    return;
+  }
+  next();
+};
+
+router.post('/signup', requireInputs('email', 'password'), async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validate user input
+  if (!validator.isEmail(email)) {
+    res.status(400).send({ error: 'Email is not valid!' });
+    return;
+  }
+  if (password.length < 6) {
+    res
+      .status(400)
+      .send({ error: 'Password needs to be have more than 6 characters!' });
+    return;
+  }
+  const isEmailExist = !!await knex
+    .select('*')
+    .from('user_credential')
+    .where('email', email)
+    .first();
+  if (isEmailExist) {
+    res.status(400).send({ error: 'Email is already exist!' });
+    return;
+  }
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  await knex
+    .insert({ email, password: hashedPassword })
+    .into('user_credential');
+  res.status(201).send('OK');
+});
+
+module.exports = router;
