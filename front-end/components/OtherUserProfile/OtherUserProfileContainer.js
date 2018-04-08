ae2b356eaf4432aa5a065941d83cbf638885bb28
rwbarkeriv <rwbarkeriv@gmail.com>
2018-04-08 22:41:52 +0300

    Add initial screen structure.

---
+++ front-end/components/OtherUserProfile/OtherUserProfileContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import OtherUserProfile from './OtherUserProfile';
+
+class OtherUserProfileContainer extends Component {
+  state = {
+    selectedIndex: 0
+  };
+
+  updateIndex = this.updateIndex.bind(this);
+
+  updateIndex(selectedIndex) {
+    this.setState({ selectedIndex });
+  }
+
+  render() {
+    const { selectedIndex } = this.state;
+    const buttons = ['Profile', 'Friends', 'Reviews'];
+    return (
+      <OtherUserProfile
+        buttons={buttons}
+        selectedIndex={this.state.selectedIndex}
+        updateIndex={this.updateIndex}
+      />
+    );
+  }
+}
+
+export default OtherUserProfileContainer;
