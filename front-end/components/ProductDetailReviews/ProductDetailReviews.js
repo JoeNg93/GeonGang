d6b12546496c0df57f36e6347093770ced698243
Maxime Marmont <marmontm@users.noreply.github.com>
2018-04-09 03:44:54 +0300

    Improve layout of Product Reviews screen

--- front-end/components/ProductDetailReviews/ProductDetailReviews.js
+++ front-end/components/ProductDetailReviews/ProductDetailReviews.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 import { Icon, Button, Rating } from "react-native-elements";
-import commonStyles from "../../utils/styles";
 import PropTypes from "prop-types";
+import commonStyles from "../../utils/styles";
+import colorCode from "../../utils/colorCode";
 
 const ProductDetailReviews = ({ productName }) => {
   return (
@@ -11,9 +12,26 @@ const ProductDetailReviews = ({ productName }) => {
       <View style={styles.header} />
 
       {/* globalReview */}
-      <Text style={styles.globalReviewTitle}>Review for this product</Text>
-      <View style={styles.globalReviewBox}>
-        <Text style={styles.globalReviewGrade}>4.4</Text>
+      <Text
+        style={[
+          styles.globalReviewTitle,
+          styles.placeholder,
+          commonStyles.fontMontserratMedium,
+          commonStyles.colorDarkBlue
+        ]}
+      >
+        Review for this product
+      </Text>
+      <View style={[styles.globalReviewBox, styles.placeholder]}>
+        <Text
+          style={[
+            styles.globalReviewGrade,
+            commonStyles.fontMontserratLight,
+            commonStyles.colorDarkBlue
+          ]}
+        >
+          4.4
+        </Text>
         <View style={styles.globalReviewRating}>
           <Rating
             type="star"
@@ -25,14 +43,20 @@ const ProductDetailReviews = ({ productName }) => {
             // onFinishRating={this.ratingCompleted}
             style={{ paddingVertical: 10 }}
           />
-          <Text style={styles.globalReviewRatingText}>
+          <Text
+            style={[
+              styles.globalReviewRatingText,
+              commonStyles.fontMontserratLight,
+              commonStyles.colorDarkBlue
+            ]}
+          >
             Based on 18 reviews and ratings
           </Text>
         </View>
       </View>
 
       {/* userReview */}
-      <View style={styles.userReview}>
+      <View style={[styles.userReview, styles.placeholder]}>
         <Image
           source={require("../../assets/images/profile-1.jpg")}
           style={styles.userProfileImage}
@@ -47,22 +71,51 @@ const ProductDetailReviews = ({ productName }) => {
             // onFinishRating={this.ratingCompleted}
             style={{ paddingVertical: 10 }}
           />
-          <TextInput placeholder="Write your review here..." multiline={true} />
-
-          <Button title="Write review" buttonStyle={styles.btnSubmit} />
+          <View style={styles.userReviewTextbox}>
+            <TextInput
+              placeholder="Write your review here..."
+              multiline={true}
+              color={colorCode.gray}
+              style={[{ fontSize: 12 }, commonStyles.fontMontserratLight]}
+            />
+          </View>
+          <Button
+            textStyle={[
+              commonStyles.fontMontserratSemiBold,
+              { fontSize: 14, color: "#ffffff" }
+            ]}
+            title="Write review"
+            buttonStyle={styles.btnSubmit}
+          />
         </View>
       </View>
 
       {/* communityReviews */}
-      <View style={styles.communityReview}>
+      <View style={[styles.communityReview, styles.placeholder]}>
         <Image
           source={require("../../assets/images/profile-1.jpg")}
           style={styles.communityReviewImage}
         />
         <View style={styles.communityReviewContents}>
           <View style={styles.communityReviewTitle}>
-            <Text style={styles.communityReviewTitleName}>Emma Watson</Text>
-            <Text style={styles.communityReviewTitleDate}>1 month ago</Text>
+            <Text
+              style={[
+                styles.communityReviewTitleName,
+                commonStyles.fontMontserratMedium,
+                commonStyles.colorDarkBlue
+              ]}
+            >
+              Emma Watson
+            </Text>
+            <Text
+              style={[
+                styles.communityReviewTitleDate,
+                commonStyles.fontMontserratLight,
+                commonStyles.colorGray
+              ]}
+            >
+              1 month ago
+            </Text>
           </View>
           <Rating
             type="star"
@@ -74,7 +127,13 @@ const ProductDetailReviews = ({ productName }) => {
             // onFinishRating={this.ratingCompleted}
             style={{ paddingVertical: 10 }}
           />
-          <Text style={styles.communityReviewComment}>
+          <Text
+            style={[
+              styles.communityReviewComment,
+              commonStyles.fontMontserratLight,
+              commonStyles.colorDarkBlue
+            ]}
+          >
             The texture is very nice and it non sticky, non greasy. Very
             hydrating and ultras a few for my sensitive skin, I can even apply
             it around the eye area. It's a must have and 1st part of my skincare
@@ -83,8 +142,22 @@ const ProductDetailReviews = ({ productName }) => {
             night.
           </Text>
           <View style={styles.communityReviewBottom}>
-            <Button icon={{ name: "favorite-border" }} title="Helpful" />
-            <Button icon={{ name: "warning" }} title="Report" />
+            <Button
+              containerViewStyle={styles.communityReviewBottomBtnCont}
+              buttonStyle={styles.communityReviewBottomBtn}
+              color={colorCode.red}
+              icon={{ name: "favorite-border", color: colorCode.red }}
+              title="Helpful (3)"
+              textStyle={styles.communityReviewBottomBtnTxt}
+            />
+            <Button
+              containerViewStyle={styles.communityReviewBottomBtnCont}
+              buttonStyle={styles.communityReviewBottomBtn}
+              color={colorCode.lightGray}
+              icon={{ name: "warning", color: colorCode.lightGray }}
+              title="Report"
+              textStyle={styles.communityReviewBottomBtnTxt}
+            />
           </View>
         </View>
       </View>
@@ -103,7 +176,15 @@ const styles = StyleSheet.create({
     height: 70
   },
 
-  globalReviewTitle: {},
+  placeholder: {
+    paddingHorizontal: 36
+  },
+
+  // Components
+
+  globalReviewTitle: {
+    fontSize: 16
+  },
 
   globalReviewBox: {
     flex: 1,
@@ -112,11 +193,15 @@ const styles = StyleSheet.create({
     maxHeight: 80
   },
 
-  globalReviewGrade: {},
+  globalReviewGrade: {
+    fontSize: 46
+  },
 
   globalReviewRating: {},
 
-  globalReviewRatingText: {},
+  globalReviewRatingText: {
+    fontSize: 13
+  },
 
   userProfileImage: {
     resizeMode: "contain",
@@ -135,17 +220,72 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
 
+  userReviewTextbox: {
+    borderRadius: 6,
+    borderColor: colorCode.lightGray,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    minHeight: 65
+  },
+
   btnSubmit: {
     backgroundColor: "#4396DC",
-    borderRadius: 20,
-    alignSelf: "flex-end"
+    borderRadius: 25,
+    alignSelf: "flex-end",
+    paddingHorizontal: 24,
+    paddingVertical: 6
   },
+
+  communityReview: {},
 
   communityReviewImage: {
     resizeMode: "contain",
     borderRadius: 28,
     height: 56,
     width: 56
+  },
+
+  communityReviewContents: {},
+
+  communityReviewTitle: {
+    flexDirection: "row"
+  },
+
+  communityReviewTitleName: {
+    flexGrow: 1,
+    textAlign: "left",
+    fontSize: 14
+  },
+
+  communityReviewTitleDate: {
+    flexGrow: 1,
+    textAlign: "right",
+    fontSize: 12
+  },
+
+  communityReviewComment: {
+    fontSize: 12
+  },
+
+  communityReviewBottom: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "baseline"
+  },
+
+  communityReviewBottomBtnCont: {
+    marginVertical: 8,
+    marginHorizontal: 2
+  },
+
+  communityReviewBottomBtn: {
+    backgroundColor: "transparent",
+    paddingVertical: 8
+  },
+
+  communityReviewBottomBtnTxt: {
+    fontSize: 12
   }
 });
 
