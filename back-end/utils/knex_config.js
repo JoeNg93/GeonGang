3f50a3683f4ec037212895674a74406a1016e848
JoeNg93 <ntuandung93@gmail.com>
2018-04-13 13:16:30 +0300

    Make db connection dynamically

--- back-end/utils/knex_config.js
+++ back-end/utils/knex_config.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    user: 'root',
-    host: '127.0.0.1',
-    database: 'geongang',
-    password: 'root',
-    port: 3306
+    user: process.env.DB_USER || 'root',
+    host: process.env.DB_HOST || '127.0.0.1',
+    database: process.env.DB_NAME || 'geongang',
+    password: process.env.DB_PASSWORD || 'root',
+    port: process.env.DB_PORT || 3306
   }
 });
 
