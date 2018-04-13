b7aaf6d3f89db15efc592c5b9a03f3c6a0e224e5
JoeNg93 <ntuandung93@gmail.com>
2018-04-13 15:01:51 +0300

    Add a helper for using API

---
+++ front-end/utils/api.js
@@ -0,0 +1,35 @@
+import { SUCCESS, PENDING, FAIL } from '../actions/types';
+import { getAuthHeader } from './auth';
+import axios from 'axios';
+import envConfig from './env_config';
+
+export const getData = ({ actionType, urlPath }) => async dispatch => {
+  dispatch({ type: `${actionType}_${PENDING}` });
+  try {
+    const response = await axios.get(`${envConfig.baseUrl}/${urlPath}`, {
+      headers: await getAuthHeader()
+    });
+    dispatch({ type: `${actionType}_${SUCCESS}`, payload: response.data.data });
+    return response;
+  } catch (err) {
+    dispatch({ type: `${actionType}_${FAIL}`, payload: err.response.data });
+    return err.response;
+  }
+};
+
+export const postData = ({ actionType, urlPath, data }) => async dispatch => {
+  dispatch({ type: `${actionType}_${PENDING}` });
+  try {
+    const response = await axios.post(`${envConfig.baseUrl}/${urlPath}`, data, {
+      headers: await getAuthHeader()
+    });
+    dispatch({ type: `${actionType}_${SUCCESS}`, payload: response.data.data });
+    return response;
+  } catch (err) {
+    dispatch({ type: `${actionType}_${FAIL}`, payload: err.response.data });
+    return err.response;
+  }
+};
+
+export const queryGraphql = ({ actionType, queryStr }) =>
+  postData({ actionType, urlPath: 'api/graphql', data: { query: queryStr } });
