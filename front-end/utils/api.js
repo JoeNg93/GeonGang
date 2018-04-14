838c08e46bfdc2982a7a58dc9ce915109e068154
JoeNg93 <ntuandung93@gmail.com>
2018-04-14 11:52:11 +0300

    Add ability to call subsequent callback (another GET for example)

--- front-end/utils/api.js
+++ front-end/utils/api.js
@@ -17,13 +17,21 @@ export const getData = ({ actionType, urlPath }) => async dispatch => {
   }
 };
 
-export const postData = ({ actionType, urlPath, data }) => async dispatch => {
+export const postData = ({
+  actionType,
+  urlPath,
+  data,
+  successCallback
+}) => async dispatch => {
   dispatch({ type: `${actionType}_${PENDING}` });
   try {
     const response = await axios.post(`${envConfig.baseUrl}/${urlPath}`, data, {
       headers: await getAuthHeader()
     });
     dispatch({ type: `${actionType}_${SUCCESS}`, payload: response.data.data });
+    if (typeof successCallback === 'function') {
+      successCallback(dispatch);
+    }
     return response;
   } catch (err) {
     dispatch({ type: `${actionType}_${FAIL}`, payload: err.response.data });
