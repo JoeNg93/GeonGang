4ebdac24fd4008d4695c84c5712e05eb9e2d0b48
Maxime Marmont <marmontm@users.noreply.github.com>
2018-04-14 14:59:27 +0300

    Finish ProductDetail and ProductDetailReviews screens

--- front-end/components/ProductDetail/ProductDetailContainer.js
+++ front-end/components/ProductDetail/ProductDetailContainer.js
@@ -3,11 +3,40 @@ import ProductDetail from "./ProductDetail";
 
 class ProductDetailContainer extends Component {
   state = {
-    productName: "Vichy Mineral 89"
+    addedState: false
+  };
+
+  product = {
+    name: "Vichy Mineral 89",
+    imgPath: require("../../assets/images/product-4.jpg"),
+    type: "Moisturizers",
+    rating: 4,
+    ingredients:
+      "aqua / water / eau • peg/ppg/polybutylene glycol-8/5/3 glycerin • glycerin • butylene glycol • methyl gluceth-20 • carbomer • sodium hyaluronate • phenoxyethanol • caprylyl glycol • citric acid • biosaccharide gum-1",
+    numberOfReviews: 18,
+    price: "$$$"
+  };
+
+  topReview = {
+    reviewerImgPath: require("../../assets/images/profile-1.jpg"),
+    comment:
+      "The texture is very nice and it non sticky, non greasy. Very hydrating and ultras a few for my sensitive skin, I can even apply it around the eye area..."
+  };
+
+  productAddHandle = () => {
+    this.setState({ addedState: !this.state.addedState });
   };
 
   render() {
-    return <ProductDetail productName={this.state.productName} />;
+    return (
+      <ProductDetail
+        state={this.state}
+        product={this.product}
+        topReview={this.topReview}
+        onPressAddProduct={this.productAddHandle}
+        onPressReview={() => {}}
+      />
+    );
   }
 }
 
