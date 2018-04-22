afa65f7b4d90b6db3cd736aa898d52c4f5eaa1a1
JoeNg93 <ntuandung93@gmail.com>
2018-04-22 10:05:34 +0300

    Randomize scanning result

--- front-end/components/ScanningResultScreen/ScanningResultScreenContainer.js
+++ front-end/components/ScanningResultScreen/ScanningResultScreenContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import colorCode from '../../utils/colorCode';
 import ScanningResultScreen from './ScanningResultScreen';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 class ScanningResultScreenContainer extends Component {
   overallScore = {
@@ -20,19 +21,91 @@ class ScanningResultScreenContainer extends Component {
       'Your skin is thisty, tired with lots of dirts and oil. We recommend cleaning your face and moisturizing before go to bed.'
   };
 
+  MIN_POSITIVE_SCORE = 50;
+  MAX_POSITIVE_SCORE = 100;
+  MIN_NEGATIVE_SCORE = 0;
+  MAX_NEGATIVE_SCORE = 50;
+
+  randomizeScanningResult = () => {
+    const moistureScore = _.random(
+      this.MIN_POSITIVE_SCORE,
+      this.MAX_POSITIVE_SCORE,
+      true
+    );
+    const uvScore = _.random(
+      this.MIN_POSITIVE_SCORE,
+      this.MAX_POSITIVE_SCORE,
+      true
+    );
+    const dirtScore = _.random(
+      this.MIN_NEGATIVE_SCORE,
+      this.MAX_NEGATIVE_SCORE,
+      true
+    );
+    const pigmentScore = _.random(
+      this.MIN_NEGATIVE_SCORE,
+      this.MAX_NEGATIVE_SCORE,
+      true
+    );
+    const overallScore =
+      (moistureScore + uvScore - dirtScore - pigmentScore) / 2;
+    return {
+      moistureScore,
+      uvScore,
+      dirtScore,
+      pigmentScore,
+      overallScore: _.clamp(overallScore, 0, 100)
+    };
+  };
+
+  getScoreTag = overallScore => {
+    if (overallScore <= 25) {
+      return 'cautious';
+    } else if (overallScore > 25 && overallScore <= 50) {
+      return 'bad';
+    } else if (overallScore > 50 && overallScore <= 75) {
+      return 'moderate';
+    } else {
+      return 'good';
+    }
+  };
+
+  getRecommendedText = scoreTag => {
+    switch (scoreTag) {
+      case 'cautious':
+        return 'Your skin is something you really need to worry about.';
+      case 'bad':
+        return 'Your skin is just bad.';
+      case 'moderate':
+        return 'Your skin is thisty, tired with lots of dirts and oil. We recommend cleaning your face and moisturizing before go to bed.';
+      case 'good':
+        return 'Your skin is healthy and has improved so far. Keep up with the good work and continue to wash your face.';
+    }
+  };
+
   render() {
+    const {
+      moistureScore,
+      uvScore,
+      dirtScore,
+      pigmentScore,
+      overallScore
+    } = this.randomizeScanningResult();
+    const scoreTag = this.getScoreTag(overallScore);
+    const recommendedText = this.getRecommendedText(scoreTag);
+
     return (
       <ScanningResultScreen
-        score={this.overallScore.score}
-        scoreTag={this.overallScore.scoreTag}
-        scoreTagColor={this.overallScore.scoreTagColor}
-        lightVersion={this.overallScore.lightVersion}
-        displayRow={this.overallScore.displayRow}
-        moistureScore={this.skinConditionResult.moistureScore}
-        dirtScore={this.skinConditionResult.dirtScore}
-        uvScore={this.skinConditionResult.uvScore}
-        pigmentScore={this.skinConditionResult.pigmentScore}
-        recommendText={this.skinConditionResult.recommendText}
+        score={overallScore}
+        scoreTag={scoreTag}
+        scoreTagColor={colorCode[`${scoreTag}Tag`]}
+        lightVersion={false}
+        displayRow={true}
+        moistureScore={moistureScore}
+        dirtScore={dirtScore}
+        uvScore={uvScore}
+        pigmentScore={pigmentScore}
+        recommendText={recommendedText}
         onPressNextStep={this.props.onPressNextStep}
       />
     );
